c012c1d93e1623d9f4bf20d3e6c945849c713231
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-30 12:02:56 -0700

    Change spacing

--- Assets/scripts/itemColor.cs
+++ Assets/scripts/itemColor.cs
@@ -7,16 +7,16 @@ public class itemColor : MonoBehaviour
     private string colorName = "none";
     static readonly Dictionary<string, Color> colors = new Dictionary<string, Color>
     {
-        { "white", new Color32(255,255,255,255)},
-        { "blue", new Color32(31,58,166,255)},
-        { "red", new Color32(166,31,35,255)},
-        { "green", new Color32(31,166,35,255)},
-        { "purple", new Color32(104,49,159,255)},
-        { "yellow", new Color32(191,169,37,255)},
-        { "orange", new Color32(188,88,35,255)},
-        { "light blue", new Color32(33,198,221,255)},
-        { "grey", new Color32(150,150,150,255)},
-        { "pink", new Color32(162,76,153,255)},
+        { "white", new Color32(255,255,255,255) },
+        { "blue", new Color32(31,58,166,255) },
+        { "red", new Color32(166,31,35,255) },
+        { "green", new Color32(31,166,35,255) },
+        { "purple", new Color32(104,49,159,255) },
+        { "yellow", new Color32(191,169,37,255) },
+        { "orange", new Color32(188,88,35,255) },
+        { "light blue", new Color32(33,198,221,255) },
+        { "grey", new Color32(150,150,150,255) },
+        { "pink", new Color32(162,76,153,255) },
     };
 
     // Start is called before the first frame update
